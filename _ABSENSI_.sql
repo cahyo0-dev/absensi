a42a3886f068538690dcbef50960ce86dcf99e6d
Cahyo <cahyoshoyo@gmail.com>
2025-11-13 10:24:10 +0800

    clean view

--- _ABSENSI_.sql
+++ _ABSENSI_.sql
@@ -1,33 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Nov 13, 2025 at 01:28 AM
--- Server version: 8.0.30
--- PHP Version: 8.1.10
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `absensi`
---
 CREATE DATABASE IF NOT EXISTS `absensi` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `absensi`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `absensis`
---
 
 CREATE TABLE `absensis` (
   `id` bigint UNSIGNED NOT NULL,
@@ -42,35 +18,17 @@ CREATE TABLE `absensis` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `cache`
---
-
 CREATE TABLE `cache` (
   `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `value` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `expiration` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `cache_locks`
---
-
 CREATE TABLE `cache_locks` (
   `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `owner` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `expiration` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `inspeksis`
---
 
 CREATE TABLE `inspeksis` (
   `id` bigint UNSIGNED NOT NULL,
@@ -82,12 +40,6 @@ CREATE TABLE `inspeksis` (
   `kategori_id` bigint UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `jawabans`
---
-
 CREATE TABLE `jawabans` (
   `id` bigint UNSIGNED NOT NULL,
   `inspeksi_id` bigint UNSIGNED NOT NULL,
@@ -97,12 +49,6 @@ CREATE TABLE `jawabans` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `kategori_inspeksis`
---
-
 CREATE TABLE `kategori_inspeksis` (
   `id` bigint UNSIGNED NOT NULL,
   `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -111,23 +57,11 @@ CREATE TABLE `kategori_inspeksis` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `migrations`
---
-
 CREATE TABLE `migrations` (
   `id` int UNSIGNED NOT NULL,
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pertanyaan`
---
 
 CREATE TABLE `pertanyaan` (
   `id` bigint UNSIGNED NOT NULL,
@@ -138,12 +72,6 @@ CREATE TABLE `pertanyaan` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `sessions`
---
-
 CREATE TABLE `sessions` (
   `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` bigint UNSIGNED DEFAULT NULL,
@@ -152,12 +80,6 @@ CREATE TABLE `sessions` (
   `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `last_activity` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` bigint UNSIGNED NOT NULL,
@@ -180,149 +102,74 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `absensis`
---
 ALTER TABLE `absensis`
   ADD PRIMARY KEY (`id`),
   ADD KEY `absensis_nip_index` (`nip`),
   ADD KEY `absensis_waktu_absen_index` (`waktu_absen`);
 
---
--- Indexes for table `cache`
---
 ALTER TABLE `cache`
   ADD PRIMARY KEY (`key`);
 
---
--- Indexes for table `cache_locks`
---
 ALTER TABLE `cache_locks`
   ADD PRIMARY KEY (`key`);
 
---
--- Indexes for table `inspeksis`
---
 ALTER TABLE `inspeksis`
   ADD PRIMARY KEY (`id`),
   ADD KEY `inspeksis_pengawas_id_foreign` (`pengawas_id`);
 
---
--- Indexes for table `jawabans`
---
 ALTER TABLE `jawabans`
   ADD PRIMARY KEY (`id`),
   ADD KEY `jawabans_inspeksi_id_foreign` (`inspeksi_id`),
   ADD KEY `jawabans_pertanyaan_id_foreign` (`pertanyaan_id`);
 
---
--- Indexes for table `kategori_inspeksis`
---
 ALTER TABLE `kategori_inspeksis`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `migrations`
---
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `pertanyaan`
---
 ALTER TABLE `pertanyaan`
   ADD PRIMARY KEY (`id`),
   ADD KEY `pertanyaan_kategori_id_foreign` (`kategori_id`);
 
---
--- Indexes for table `sessions`
---
 ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`),
   ADD KEY `sessions_user_id_index` (`user_id`),
   ADD KEY `sessions_last_activity_index` (`last_activity`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `absensis`
---
 ALTER TABLE `absensis`
   MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `inspeksis`
---
 ALTER TABLE `inspeksis`
   MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `jawabans`
---
 ALTER TABLE `jawabans`
   MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `kategori_inspeksis`
---
 ALTER TABLE `kategori_inspeksis`
   MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `migrations`
---
 ALTER TABLE `migrations`
   MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `pertanyaan`
---
 ALTER TABLE `pertanyaan`
   MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
   MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `inspeksis`
---
 ALTER TABLE `inspeksis`
   ADD CONSTRAINT `inspeksis_pengawas_id_foreign` FOREIGN KEY (`pengawas_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
---
--- Constraints for table `jawabans`
---
 ALTER TABLE `jawabans`
   ADD CONSTRAINT `jawabans_inspeksi_id_foreign` FOREIGN KEY (`inspeksi_id`) REFERENCES `inspeksis` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `jawabans_pertanyaan_id_foreign` FOREIGN KEY (`pertanyaan_id`) REFERENCES `pertanyaan` (`id`) ON DELETE CASCADE;
 
---
--- Constraints for table `pertanyaan`
---
 ALTER TABLE `pertanyaan`
   ADD CONSTRAINT `pertanyaan_kategori_id_foreign` FOREIGN KEY (`kategori_id`) REFERENCES `kategori_inspeksis` (`id`) ON DELETE CASCADE;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;
